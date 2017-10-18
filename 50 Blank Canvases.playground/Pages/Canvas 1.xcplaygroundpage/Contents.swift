e3d1d0d0802ecd863489157f788d492d8b3ce8ec
Hofmann, Hannah <hhofmann@lcs-mba3162.lcs.on.ca>
2017-10-18 11:59:09 -0400

    I drew a flower

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 500, height: 500)
 
 /*:
  ## Add your code below
@@ -22,10 +22,63 @@ let canvas = Canvas(width: 400, height: 300)
  
  Use whitespace and comments as appropriate.
  */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+//draw a flower
 
+// draw the petals
+canvas.lineColor = Color.red
+for x in stride(from: 100, through: 150, by: 50) {
+    canvas.drawLine(fromX: 50, fromY: 450, toX: x, toY: 350)
+}
+for x in stride(from: 150, through: 200, by: 50) {
+    canvas.drawLine(fromX: 100, fromY: 450, toX: x, toY: 350)
+}
+for x in stride(from: 150, through: 250, by: 100) {
+    canvas.drawLine(fromX: 200, fromY: 450, toX: x, toY: 350)
+}
+for x in stride(from: 200, through: 250, by: 50) {
+    canvas.drawLine(fromX: 300, fromY: 450, toX: x, toY: 350)
+}
+for x in stride(from: 250, through: 300, by: 50) {
+    canvas.drawLine(fromX: 350, fromY: 450, toX: x, toY: 350)
+}
 
+//draw the base of the flower
+canvas.lineColor = Color.green
+for x in stride(from: 150, through: 175, by: 25) {
+    canvas.drawLine(fromX: 100, fromY: 350, toX: x, toY: 300)
+}
+for x in stride(from: 175, through: 200, by: 25) {
+    canvas.drawLine(fromX: 150, fromY: 350, toX: x, toY: 300)
+}
+for x in stride(from: 175, through: 225, by: 50) {
+    canvas.drawLine(fromX: 200, fromY: 350, toX: x, toY: 300)
+}
+for x in stride(from: 200, through: 225, by: 25) {
+    canvas.drawLine(fromX: 250, fromY: 350, toX: x, toY: 300)
+}
+for x in stride(from: 225, through: 250, by: 25) {
+    canvas.drawLine(fromX: 300, fromY: 350, toX: x, toY: 300)
+}
+canvas.drawLine(fromX: 250, fromY: 300, toX: 200, toY: 250)
+canvas.drawLine(fromX: 150, fromY: 300, toX: 200, toY: 250)
 
+//draw the stem
+for x in stride(from: 190, through: 210, by: 20) {
+    canvas.drawLine(fromX: 200, fromY: 250, toX: x, toY: 200)
+}
+for x in stride(from: 190, through: 210, by: 20) {
+    canvas.drawLine(fromX: 200, fromY: 150, toX: x, toY: 200)
+}
+for x in stride(from: 190, through: 210, by: 20) {
+    canvas.drawLine(fromX: 200, fromY: 150, toX: x, toY: 100)
+}
+for x in stride(from: 190, through: 210, by: 20) {
+    canvas.drawLine(fromX: 200, fromY: 50, toX: x, toY: 100)
+}
+
+for x in stride(from: 190, through: 210, by: 20) {
+    canvas.drawLine(fromX: 200, fromY: 50, toX: x, toY: 0)
+}
 /*:
  ## Use source control
  To keep your work organized, and receive feedback, source control is a must.
